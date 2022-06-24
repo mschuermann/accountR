b1897622c71918c17b0f27b4d7fd17abd74cbec7
Marie Schürmann <schrmanm@tcd.ie>
2022-06-24 10:23:49 +0100

    formatting of the documentation

--- R/to_home_currency.R
+++ R/to_home_currency.R
@@ -3,38 +3,43 @@
 
 #' Converts foreign currencies in a data frame to a chosen currency by the user
 #'
-#' \code{FX_rate_convert} downloads foreign exchange courses from the European Central Bank's API to convert foreign currencies to a chosen currency by the user and documents the exchange rate used.
+#' \code{FX_rate_convert} downloads foreign exchange courses from the European
+#' Central Bank's API to convert foreign currencies to a chosen currency by the
+#' user and documents the exchange rate used.
 #'
 #' @param data \emph{a data frame} including columns indicating the following:
-#'   foreign exchange course identifier and the amounts to be translated into the new currency.
+#'   foreign exchange course identifier and the amounts to be translated into
+#'   the new currency.
 #'
-#' @param FC_column \emph{a column name} in the data frame indicating the foreign exchange course identifier. See \code{priceR::currencies()} for supported currency codes.
+#' @param FC_column \emph{a column name} in the data frame indicating the
+#' foreign exchange course identifier. See \code{priceR::currencies()} for
+#' supported currency codes.
 #'
-#' @param amount \emph{a column name} in the data frame indicating the amounts which should be translated into another currency.
+#' @param amount \emph{a column name} in the data frame indicating the amounts
+#' which should be translated into another currency.
 #'
-#' @param new_currency \emph{a currency code} indicating which currency the amounts should be translate into. See \code{priceR::currencies()} for supported currency codes.
+#' @param new_currency \emph{a currency code} indicating which currency the
+#' amounts should be translate into. See \code{priceR::currencies()} for
+#' supported currency codes.
 #'
-#' @param report_date \emph{a date} to which the amounts should be translated into another currency. The default is the system date. The format must be
+#' @param report_date \emph{a date} to which the amounts should be translated
+#' into another currency. The default is the system date. The format must be
 #'   "YYYY-MM-DD".
 #'
 #' @return a dataframe which has two additional columns: \describe{
-#'   \item{\strong{FX_rate}}{the applied exchange rate} \item{\strong{translated_amount}}{the amount translated to the chosen currency} }
+#'   \item{\strong{FX_rate}}{the applied exchange rate}
+#'   \item{\strong{translated_amount}}{the amount translated to the chosen currency}
+#'   }
 #'
 #' @examples
-#' currencies <- data.frame(
-#'   currency = c("USD", "DKK", "USD", NA),
-#'   price = c(100, 329, 23, 799),
-#'   date = as.Date(c("2020-01-01", "2020-02-01", "2020-03-01", "2020-03-01"))
-#' )
-#'
-#' FX_rate_convert(data = currencies, amount = "price", FC_column = "currency", new_currency = "EUR")
-#' FX_rate_convert(data = currencies, amount = "price", FC_column = "currency", new_currency = "USD", report_date = "2022-06-30")
+#' example <- accountR::demo_file
+#' FX_rate_convert(data = example, FC_column = "currency", amount = "amount", new_currency = "EUR", report_date = "2022-06-30")
 #'
 FX_rate_convert <- function(data,
                             FC_column,
                             amount,
                             new_currency,
-                            report_date = as.Date(Sys.Date())) {
+                            report_date) {
   data[[FC_column]] <-
     tidyr::replace_na(data[[FC_column]], new_currency)
   official_currency_codes <- priceR::currencies()
