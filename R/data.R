b5c9e484e63e841fb883f45c43ad02e38cccf04b
Marie Schürmann <schrmanm@tcd.ie>
2022-06-29 17:18:16 +0100

    Demo file for vignette and examples in documentation

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Demo file to understand the outputs of the functions.
+#'
+#' A data set containing a customer open-invoices report with the typical data
+#' included, i.e. customer name and ID, invoice number and open amount, currencies.
+#'
+#' @format A data frame with 500 rows and 8 variables:
+#' \describe{
+#'   \item{company name}{the name of the customer company}
+#'   \item{country}{country where the company is operating}
+#'   \item{customer id}{each individual entity has its own individual customer ID}
+#'   \item{invoice date}{each row represents an individual invoice}
+#'   \item{invoice no}{each row represents an individual invoice}
+#'   \item{currency}{currency code for each invoice}
+#'   \item{amount}{open amount of the invoice}
+#'   \item{due date}{date when the invoice is due to be paid}
+#'
+#' }
+#'
+#' @source The data set is fictitious and every similarity to other company's data is coincidentally.
+#' @keywords datasets
+"demo_file"
